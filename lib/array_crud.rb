d0058d7360a1faca0cae58915ed289fd09336e43
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-19 10:57:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,12 +6,12 @@ def
   create_an_array = ["Minh", "Hoang", "Nam", "Namek"]
 end
 
-def #add_element_to_end_of_array(array, element)
+def add_element_to_end_of_array(array, element)
   create_an_array << "Mi"
   create_an_array.push("My") 
 end
 
-def #add_element_to_start_of_array(array, element)
+def add_element_to_start_of_array(array, element)
   create_an_array.unsfift("Hiep")
 end
 
